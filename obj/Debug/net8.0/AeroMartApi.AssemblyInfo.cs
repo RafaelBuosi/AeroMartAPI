743fb53e4f5ff998edda4475f6e02fdff0f2714f
Rafael Buosi <rbjardim2005@gmail.com>
2026-06-01 16:14:51 -0300

    AeroMart: Adicionado Toast de sucesso, validacao de estoque e correcao de atualizacao da tabela

--- obj/Debug/net8.0/AeroMartApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/AeroMartApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeroMartApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ea86ab0c5f395d7788d33178ab9fe45e73fc64")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeroMartApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeroMartApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
